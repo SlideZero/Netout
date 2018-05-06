b90f1f6362ba257952db79b80a64be7ff8c8db6a
ZeroSlide <kibashoten@gmail.com>
2018-05-06 15:23:57 -0500

    Checkpoints
    Agregado del funcionamiento de los checkpoints

--- Netout/Assets/Scripts/UINicknameController.cs
+++ Netout/Assets/Scripts/UINicknameController.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class UINicknameController : MonoBehaviour {
 
@@ -11,6 +12,11 @@ public class UINicknameController : MonoBehaviour {
 	public GameObject _nicknameLayer;
 	public Text _nickname;
 	public string _nicknameString;
+	public string menuSceneName;
+	public GameObject _loadingScreen;
+	public Slider _slider;
+	public Text _progressText;
+	private bool inGame = false;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +29,11 @@ public class UINicknameController : MonoBehaviour {
 		{
 			_camera.GetComponent<IntroductionCamera>().enabled = false;
 			_camera.GetComponent<CameraFollow360>().enabled = true;
+			inGame = true;
 		}
+
+		if((!NetworkServer.active && !NetworkClient.active) && inGame)
+			StartCoroutine(LoadAsyncronously(menuSceneName));
 	}
 
 	public void SetNickname()
@@ -31,5 +41,18 @@ public class UINicknameController : MonoBehaviour {
 		_nWManager.GetComponent<NetworkManagerHUD>().enabled = true;
 		_nicknameString = _nickname.text;
 		_nicknameLayer.SetActive(false);
+	}
+
+	IEnumerator LoadAsyncronously(string _nextScene)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(_nextScene);
+		_loadingScreen.SetActive(true);
+		while(!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+			_slider.value = progress;
+			_progressText.text = progress * 100f + "%";
+			yield return null;
+		}
 	}
 }
